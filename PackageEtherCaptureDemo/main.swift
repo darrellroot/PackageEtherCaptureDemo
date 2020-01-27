fb1a6f2c858279e781db0cfdc06dbf7177ca7303
Darrell Root <darrellrootjunk@mac.com>
2020-01-27 13:44:44 -0800

    Initial working version as a code sample

--- PackageEtherCaptureDemo/main.swift
+++ PackageEtherCaptureDemo/main.swift
@@ -7,6 +7,14 @@
 //
 
 import Foundation
+import PackageEtherCapture
 
-print("Hello, World!")
-
+let etherCapture: EtherCapture?
+do {
+    etherCapture = try EtherCapture(interface: "en0", command: "icmp or icmp6") { frame in
+        debugPrint(frame.description)
+    }
+} catch {
+    print("EtherCapture initialization failed with error \(error)")
+}
+RunLoop.current.run()
